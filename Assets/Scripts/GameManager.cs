09c801b99b31c611c957710e0acbefc657872e00
syuukouha <252747500@qq.com>
2017-03-08 16:40:50 +0900

    Firstcommit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get { return _instance; }
+    }
+
+    public string VideoFileName;
+    void Awake()
+    {
+        _instance = this;
+    }
+	// Use this for initialization
+	void Start ()
+	{
+	    DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    private Image _image;
+	// Use this for initialization
+	void Start ()
+	{
+	    _image = GetComponent<Image>();
+	    _image.fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _image.fillAmount += Time.deltaTime;
+	    if (_image.fillAmount >= 1.0f)
+	    {
+	        SceneManager.LoadSceneAsync(1);
+	    }
+	}
+}
