c97a268295eb230764a43d3976a601ed04cd7a14
syuukouha <252747500@qq.com>
2017-06-21 17:33:28 +0900

    Add the Guide ButtonClickEvent

--- Assets/Scripts/Guide.cs
+++ Assets/Scripts/Guide.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Guide : MonoBehaviour {
 
 	// Use this for initialization
@@ -9,4 +9,8 @@ public class Guide : MonoBehaviour {
     {
         PlayerPrefs.SetInt("Guide", 1);
 	}
+    public void OnClick()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
