ee3f1bfd867e010e3e865c27421db9e599448c7e
meganeroppy <roppy@hotmail.co.jp>
2017-10-06 17:31:16 +0900

    シーン遷移用クラスを作成

---
+++ Assets/Scripts/VideoPlayTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoPlayTrigger : MonoBehaviour 
+{
+	[SerializeField]
+	private VideoClip clip;
+
+	private void OnEnable()
+	{
+		Debug.Log(System.Reflection.MethodBase.GetCurrentMethod());
+
+		if( !GameManager.Instance )
+		{
+			Debug.LogError("GameManager is null");
+			return;
+		}
+
+		GameManager.Instance.VideoFileName = clip.name;
+		SceneManager.LoadSceneAsync("VR");
+	}
+
+}
