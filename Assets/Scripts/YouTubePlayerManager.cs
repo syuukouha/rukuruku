21fa21fd8686d9834e8ff0d246284549b5500afa
meganeroppy <roppy@hotmail.co.jp>
2017-11-10 01:08:41 +0900

    とりあえずYouTubeの動画（普通の動画＆360）の再生に対応 ただしカメラモードの切り替えや中断がまだ使えない

---
+++ Assets/Scripts/YouTubePlayerManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouTubePlayerManager : MonoBehaviour {
+
+	[SerializeField]
+	private HighQualityPlayback youtubePayer;
+
+	/// <summary>
+	/// GameManagerに値あ設定されていなければこれを使う
+	/// </summary>
+	[SerializeField]
+	private string videoFileName;
+
+
+	/// <summary>
+	/// HighQualityPlayback の Start() より前に呼ばれる必要がある
+	/// </summary>
+	void Awake()
+	{
+		string fileName = null;
+		if( !GameManager.Instance || string.IsNullOrEmpty( GameManager.Instance.VideoFileName )) 
+		{
+			Debug.LogError("GameManagerが存在しない または VideoFileNameが未定義 " + videoFileName + "を使用する");
+			fileName = videoFileName;
+		}
+		else
+		{
+			fileName = GameManager.Instance.VideoFileName;
+		}
+
+		youtubePayer.videoId = fileName;
+	}
+
+}
