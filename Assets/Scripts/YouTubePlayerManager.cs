8ab916ba5ff975bae825d5f146fb01bf94208d5d
meganeroppy <roppy@hotmail.co.jp>
2017-11-10 01:39:46 +0900

    YouTubeの360度動画用シーンを調整

--- Assets/Scripts/YouTubePlayerManager.cs
+++ Assets/Scripts/YouTubePlayerManager.cs
@@ -13,12 +13,32 @@ public class YouTubePlayerManager : MonoBehaviour {
 	[SerializeField]
 	private string videoFileName;
 
+	/// <summary>
+	/// １つのカメラで動画を見る時に使用するオブジェクト
+	/// </summary>
+	[SerializeField]
+	private GameObject[] singleCameraObjects;
+
+	/// <summary>
+	/// ２つのカメラで動画を見る時に使用するオブジェクト
+	/// </summary>
+	[SerializeField]
+	private GameObject[] dualCameraObjects;
+
+	/// <summary>
+	/// 再生停止処理中か？
+	/// </summary>
+	private bool inQuitProcess = false;
 
 	/// <summary>
 	/// HighQualityPlayback の Start() より前に呼ばれる必要がある
 	/// </summary>
 	void Awake()
 	{
+		inQuitProcess = false;
+
+		LoadCameraMode();
+
 		string fileName = null;
 		if( !GameManager.Instance || string.IsNullOrEmpty( GameManager.Instance.VideoFileName )) 
 		{
@@ -31,6 +51,76 @@ public class YouTubePlayerManager : MonoBehaviour {
 		}
 
 		youtubePayer.videoId = fileName;
+	}
+
+	/// <summary>
+	/// カメラモード読み込み
+	/// </summary>
+	private void LoadCameraMode()
+	{
+		string key = "CameraMode";
+		int cameraMode = 1;
+		if (PlayerPrefs.HasKey(key))
+		{
+			cameraMode = PlayerPrefs.GetInt(key);
+		}
+
+		SetCameraModeObjects(cameraMode);
+	}
+
+	/// <summary>
+	/// カメラモードでオブジェクトの有効/無効を切り替え
+	/// </summary>
+	private void SetCameraModeObjects(int mode)
+	{
+		// いったんすべて無効
+		foreach (GameObject o in singleCameraObjects)
+		{
+			o.SetActive(false);
+		}
+		foreach (GameObject o in dualCameraObjects)
+		{
+			o.SetActive(false);
+		}
+
+		// モードに応じて対応オブジェクトを有効にする
+		if (mode == 1)
+		{
+			foreach (GameObject o in singleCameraObjects)
+			{
+				o.SetActive(true);
+			}
+		}
+		else
+		{
+			foreach (GameObject o in dualCameraObjects)
+			{
+				o.SetActive(true);
+			}
+		}
+	}
+
+	/// <summary>
+	/// カメラモードの切り替え
+	/// 現在のモードをセーブ
+	/// </summary>
+	public void SwitchCameraMode(int newMode)
+	{
+		SetCameraModeObjects(newMode);
+
+		PlayerPrefs.SetInt("CameraMode", newMode);
+	}
+
+	/// <summary>
+	/// ARシーンに戻る
+	/// </summary>
+	public void ReturnToARScene()
+	{
+		if( inQuitProcess ) return;
+
+		inQuitProcess = true;
+
+		UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("AR");
 	}
 
 }
