f90684d24f6e821e7cb484d83872675da2e518a5
meganeroppy <roppy@hotmail.co.jp>
2017-11-14 21:31:48 +0900

    アルファ付き動画を再生しようとがんばってるけど、まだできない！

---
+++ Assets/Scripts/TrackedScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+/// <summary>
+/// トラッキングされる動画制御
+/// </summary>
+public class TrackedScreen : MonoBehaviour 
+{
+	[SerializeField]
+	private VideoPlayer videoPlayer;
+
+	// Use this for initialization
+	void Awake () {
+		videoPlayer.url = GameManager.Instance.VideoFileName;
+
+		// ここで、
+		// Keep Alpha = true
+		// とか、
+		// Transcode = true
+		// とか、できたらいいね？
+
+	}
+}
